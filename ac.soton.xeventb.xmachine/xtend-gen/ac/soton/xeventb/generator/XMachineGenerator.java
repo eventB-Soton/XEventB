62ce3d3fb22fc970c2e0ae658cebe448f3d3a1bd
Thái Sơn Hoàng <T.S.Hoang@ecs.soton.ac.uk>
2017-04-03 20:52:00 +0100

    Rename UI project
    For consistency, the shared UI project is renamed from ac.soton.eventb.xtext.ui to ac.soton.xeventb.ui

--- ac.soton.xeventb.xmachine/xtend-gen/ac/soton/xeventb/generator/XMachineGenerator.java
+++ ac.soton.xeventb.xmachine/xtend-gen/ac/soton/xeventb/generator/XMachineGenerator.java
@@ -10,11 +10,14 @@
  */
 package ac.soton.xeventb.generator;
 
+import ac.soton.emf.translator.TranslatorFactory;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.generator.IFileSystemAccess;
 import org.eclipse.xtext.generator.IGenerator;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eventb.emf.core.machine.Machine;
 import org.eventb.emf.persistence.EMFRodinDB;
 
@@ -30,13 +33,28 @@ public class XMachineGenerator implements IGenerator {
    */
   @Override
   public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
-    EObject _get = resource.getContents().get(0);
-    Machine mch = ((Machine) _get);
-    EMFRodinDB emfRodinDB = new EMFRodinDB();
-    String uriString = resource.getURI().toString();
-    uriString = uriString.substring(0, uriString.lastIndexOf("bumx"));
-    uriString = (uriString + "bum");
-    URI uri = URI.createURI(uriString);
-    emfRodinDB.saveResource(uri, mch);
+    try {
+      EObject _get = resource.getContents().get(0);
+      Machine mch = ((Machine) _get);
+      boolean _isEmpty = mch.getExtensions().isEmpty();
+      if (_isEmpty) {
+        EMFRodinDB emfRodinDB = new EMFRodinDB();
+        String uriString = resource.getURI().toString();
+        uriString = uriString.substring(0, uriString.lastIndexOf("bumx"));
+        uriString = (uriString + "bum");
+        URI uri = URI.createURI(uriString);
+        emfRodinDB.saveResource(uri, mch);
+      } else {
+        String commandId = "ac.soton.eventb.emf.inclusion.commands.include";
+        TranslatorFactory _factory = TranslatorFactory.getFactory();
+        TranslatorFactory factory = ((TranslatorFactory) _factory);
+        if (((factory != null) && factory.canTranslate(commandId, mch.eClass()))) {
+          NullProgressMonitor _nullProgressMonitor = new NullProgressMonitor();
+          factory.translate(mch, commandId, _nullProgressMonitor);
+        }
+      }
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 }
