004cdb80152847bf453af07c0bd668d9fa81083f
Thái Sơn Hoàng <T.S.Hoang@ecs.soton.ac.uk>
2020-06-01 19:27:26 +0100

    XContext: Show warnings for untranslated formulae

--- ac.soton.xeventb.xcontext/src/ac/soton/xeventb/xcontext/validation/XContextValidator.xtend
+++ ac.soton.xeventb.xcontext/src/ac/soton/xeventb/xcontext/validation/XContextValidator.xtend
@@ -1,37 +1,74 @@
 /*******************************************************************************
- * Copyright (c) 2017,2019 University of Southampton.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ *  Copyright (c) 2017, 2020 University of Southampton.
  *
- * Contributors:
- *     University of Southampton - initial API and implementation
+ *  This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License 2.0
+ *  which accompanies this distribution, and is available at
+ *  https://www.eclipse.org/legal/epl-2.0/
+ *
+ *  SPDX-License-Identifier: EPL-2.0
+ *
+ *  Contributors:
+ *    University of Southampton - initial API and implementation
  *******************************************************************************/
+
 package ac.soton.xeventb.xcontext.validation
 
+import ac.soton.xeventb.common.IValidationIssueCode
 import org.eclipse.xtext.validation.Check
+import org.eventb.emf.core.CorePackage
+import org.eventb.emf.core.EventBPredicate
 import org.eventb.emf.core.context.Context
+import org.rodinp.keyboard.core.RodinKeyboardCore
 
 /**
  * <p>
- * XContext validator, provides custom validation rules for the xtext context file.
+ * XContext validator, provides custom validation rules for the XContexts.
  * </p>
  *
- * @author dana
+ * @author dana - Initial Implementation
  * @version 0.1 
  * @since 1.0
  */
-
-
 class XContextValidator extends AbstractXContextValidator {
 	
-   @Check
-	def checkMachineName(Context ctx){
+   	/**
+   	 * Check to ensure that the context name match the name of the file.
+   	 * 
+   	 * @author dana
+   	 * @author htson - Renamed from "checkMachineName"
+   	 * @since 2.0
+   	 */
+   	@Check
+	def checkContextName(Context ctx){
 		val res = ctx.eResource
 		val fileName = res.URI.lastSegment.toString
 		val ctxName= fileName.substring(0, fileName.indexOf('.'))
 		if (ctxName != ctx.name)
 			error('Context name should be the same as the file name', null) 
 	}
+
+	/**
+	 * Check for untranslated predicates by comparing the translated string
+	 * with the predicate. Raise a warning with code
+	 * {@link IValidationIssueCode#UNTRANSLATE_PREDICATE}. The code is used for
+	 * providing quick fixes.
+	 * 
+	 * @param obj 
+	 * 		an Event-B predicate EObject.
+	 * @author htson
+	 * @see IValidationIssueCode
+	 * @since 2.0
+	 */
+	@Check
+	def untranslatedPredicate(EventBPredicate obj) {
+		val predicate = obj.predicate
+		val translated = RodinKeyboardCore.translate(predicate)
+		if (predicate != translated)
+			warning("Untranslated Predicate: " + predicate, obj,
+				CorePackage.Literals.EVENT_BPREDICATE__PREDICATE,
+				IValidationIssueCode.UNTRANSLATED_PREDICATE, predicate, translated
+			)
+	}
+
 }
--- ac.soton.xeventb.xcontext/xtend-gen/ac/soton/xeventb/xcontext/validation/XContextValidator.java
+++ ac.soton.xeventb.xcontext/xtend-gen/ac/soton/xeventb/xcontext/validation/XContextValidator.java
@@ -1,34 +1,48 @@
 /**
- * Copyright (c) 2017,2019 University of Southampton.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2017, 2020 University of Southampton.
  * 
- * Contributors:
- *     University of Southampton - initial API and implementation
+ *  This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License 2.0
+ *  which accompanies this distribution, and is available at
+ *  https://www.eclipse.org/legal/epl-2.0/
+ * 
+ *  SPDX-License-Identifier: EPL-2.0
+ * 
+ *  Contributors:
+ *    University of Southampton - initial API and implementation
  */
 package ac.soton.xeventb.xcontext.validation;
 
+import ac.soton.xeventb.common.IValidationIssueCode;
 import ac.soton.xeventb.xcontext.validation.AbstractXContextValidator;
 import com.google.common.base.Objects;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.validation.Check;
+import org.eventb.emf.core.CorePackage;
+import org.eventb.emf.core.EventBPredicate;
 import org.eventb.emf.core.context.Context;
+import org.rodinp.keyboard.core.RodinKeyboardCore;
 
 /**
  * <p>
- * XContext validator, provides custom validation rules for the xtext context file.
+ * XContext validator, provides custom validation rules for the XContexts.
  * </p>
  * 
- * @author dana
+ * @author dana - Initial Implementation
  * @version 0.1
  * @since 1.0
  */
 @SuppressWarnings("all")
 public class XContextValidator extends AbstractXContextValidator {
+  /**
+   * Check to ensure that the context name match the name of the file.
+   * 
+   * @author dana
+   * @author htson - Renamed from "checkMachineName"
+   * @since 2.0
+   */
   @Check
-  public void checkMachineName(final Context ctx) {
+  public void checkContextName(final Context ctx) {
     final Resource res = ctx.eResource();
     final String fileName = res.getURI().lastSegment().toString();
     final String ctxName = fileName.substring(0, fileName.indexOf("."));
@@ -36,6 +50,30 @@ public class XContextValidator extends AbstractXContextValidator {
     boolean _notEquals = (!Objects.equal(ctxName, _name));
     if (_notEquals) {
       this.error("Context name should be the same as the file name", null);
+    }
+  }
+  
+  /**
+   * Check for untranslated predicates by comparing the translated string
+   * with the predicate. Raise a warning with code
+   * {@link IValidationIssueCode#UNTRANSLATE_PREDICATE}. The code is used for
+   * providing quick fixes.
+   * 
+   * @param obj
+   * 		an Event-B predicate EObject.
+   * @author htson
+   * @see IValidationIssueCode
+   * @since 2.0
+   */
+  @Check
+  public void untranslatedPredicate(final EventBPredicate obj) {
+    final String predicate = obj.getPredicate();
+    final String translated = RodinKeyboardCore.translate(predicate);
+    boolean _notEquals = (!Objects.equal(predicate, translated));
+    if (_notEquals) {
+      this.warning(("Untranslated Predicate: " + predicate), obj, 
+        CorePackage.Literals.EVENT_BPREDICATE__PREDICATE, 
+        IValidationIssueCode.UNTRANSLATED_PREDICATE, predicate, translated);
     }
   }
 }
