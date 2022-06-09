66bc415e82f1dbc8c3f35d3995ac198ebe2689af
Thái Sơn Hoàng <T.S.Hoang@ecs.soton.ac.uk>
2022-06-09 09:57:07 +0100

    XContext: Commented the scope provider for records
    Record relationship are now Strings rather than references

--- ac.soton.xeventb.xcontext/xtend-gen/ac/soton/xeventb/xcontext/scoping/XContextScopeProvider.java
+++ ac.soton.xeventb.xcontext/xtend-gen/ac/soton/xeventb/xcontext/scoping/XContextScopeProvider.java
@@ -13,23 +13,17 @@
  */
 package ac.soton.xeventb.xcontext.scoping;
 
-import ac.soton.eventb.emf.record.Record;
-import ac.soton.eventb.emf.record.RecordPackage;
-import ac.soton.xeventb.xcontext.scoping.AbstractXContextScopeProvider;
 import ch.ethz.eventb.utils.EventBUtils;
 import com.google.common.base.Objects;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.Scopes;
 import org.eclipse.xtext.xbase.lib.Exceptions;
@@ -37,10 +31,8 @@ import org.eventb.core.IContextRoot;
 import org.eventb.core.IEventBProject;
 import org.eventb.core.basis.ContextRoot;
 import org.eventb.emf.core.EventBElement;
-import org.eventb.emf.core.EventBObject;
 import org.eventb.emf.core.context.Context;
 import org.eventb.emf.core.context.ContextPackage;
-import org.eventb.emf.core.machine.Machine;
 import org.rodinp.core.IInternalElement;
 import org.rodinp.core.IRodinProject;
 
@@ -82,42 +74,10 @@ public class XContextScopeProvider extends AbstractXContextScopeProvider {
         }
         return Scopes.scopeFor(ctxs);
       }
-      if (((context instanceof Record) && Objects.equal(reference, RecordPackage.Literals.RECORD__SUBSETS))) {
-        EObject _rootContainer = EcoreUtil2.getRootContainer(context, true);
-        final Context cntx = ((Context) _rootContainer);
-        final List<EventBObject> components = this.getComponentsInScope(cntx);
-        final List<Record> records = EcoreUtil2.<Record>getAllContentsOfType(cntx, Record.class);
-        for (final EventBObject c : components) {
-          records.addAll(EcoreUtil2.<Record>getAllContentsOfType(((EObject) c), Record.class));
-        }
-        return Scopes.scopeFor(records);
-      }
       return null;
     } catch (Throwable _e) {
       throw Exceptions.sneakyThrow(_e);
     }
-  }
-  
-  private List<EventBObject> getComponentsInScope(final EventBObject eventBObject) {
-    ArrayList<EventBObject> list = new ArrayList<EventBObject>();
-    if ((eventBObject instanceof Machine)) {
-      Machine m = ((Machine) eventBObject);
-      list.add(m);
-      EList<Context> _sees = m.getSees();
-      for (final Context c : _sees) {
-        list.addAll(this.getComponentsInScope(c));
-      }
-    } else {
-      if ((eventBObject instanceof Context)) {
-        Context c_1 = ((Context) eventBObject);
-        list.add(c_1);
-        EList<Context> _extends = c_1.getExtends();
-        for (final Context x : _extends) {
-          list.addAll(this.getComponentsInScope(x));
-        }
-      }
-    }
-    return list;
   }
   
   /**
